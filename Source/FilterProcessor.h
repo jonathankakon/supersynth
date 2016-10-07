e092cc1eb23956d17f1483baf72f5e12eae8613e
pjlehmann <pj.lehmann@mac.com>
2016-10-07 14:52:15 +0200

    added „Components“ and „Processors“ Folders and the FilterProcessor.h file

---
+++ Source/FilterProcessor.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    FilterProcessor.h
+    Created: 7 Oct 2016 2:50:49pm
+    Author:  Paul Lehmann
+
+  ==============================================================================
+*/
+
+#ifndef FILTERPROCESSOR_H_INCLUDED
+#define FILTERPROCESSOR_H_INCLUDED
+
+
+
+
+
+#endif  // FILTERPROCESSOR_H_INCLUDED
