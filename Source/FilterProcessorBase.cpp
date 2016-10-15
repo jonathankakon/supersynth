2eccc5cce2967bc6d609d8eb8e2c7cc9c574de68
pjlehmann <pj.lehmann@mac.com>
2016-10-15 22:44:18 +0200

    implemented all pass functionality for the FilterProcessorBaseClass

--- Source/FilterProcessorBase.cpp
+++ Source/FilterProcessorBase.cpp
@@ -9,3 +9,51 @@
 */
 
 #include "FilterProcessorBase.h"
+
+
+
+void FilterProcessorBase::resetAllpassState()
+{
+  currentAPState.xh0  = 0;
+  currentAPState.xh1  = 0;
+  currentAPState.xh2  = 0;
+}
+
+void FilterProcessorBase::applyFirstOrderAllpass(AudioBuffer<float>& buffer)
+{
+  for(int sampleIndex = 0; sampleIndex < buffer.getNumSamples(); sampleIndex++)
+  {
+    currentAPState.xh0 = buffer.getSample(0, sampleIndex) - (*firstOrderC0) * currentAPState.xh1;
+    buffer.setSample(0, sampleIndex, *firstOrderC0 * currentAPState.xh0 + currentAPState.xh1);
+    
+    currentAPState.xh2 = currentAPState.xh1;
+    currentAPState.xh1 = currentAPState.xh0;
+  }
+  
+  buffer.copyFrom(1, 0, buffer, 0, 0, buffer.getNumSamples()); // mono => right channel 0 into channel 1
+}
+
+void FilterProcessorBase::applySecondOrderAllpass(AudioBuffer<float>& buffer)
+{
+  for(int sampleIndex = 0; sampleIndex < buffer.getNumSamples(); sampleIndex++)
+  {
+    currentAPState.xh0 = (buffer.getSample(0, sampleIndex)) - (*secondOrderC1 * (1 - *secondOrderC0) * currentAPState.xh1) + (*secondOrderC0 * currentAPState.xh2);
+    
+    buffer.setSample(0, sampleIndex, (-(*secondOrderC0) * currentAPState.xh0) + (*secondOrderC1 * (1 - *secondOrderC0) * currentAPState.xh1) + currentAPState.xh2 );
+    
+    currentAPState.xh2 = currentAPState.xh1;
+    currentAPState.xh1 = currentAPState.xh0;
+  }
+  
+  buffer.copyFrom(1, 0, buffer, 0, 0, buffer.getNumSamples()); // mono => right channel 0 into channel 1
+}
+
+
+void FilterProcessorBase::updateCoefficients()
+{
+  *firstOrderC0 = (tan(double_Pi * (*currentCutOffFrequency)/sampleRate) - 1)/(tan(double_Pi * (*currentCutOffFrequency)/sampleRate) + 1);
+  
+  *secondOrderC0 = (tan(double_Pi * (*currentBandwidth)/sampleRate) - 1)/(tan(double_Pi * (*currentBandwidth)/sampleRate) + 1);
+  
+  *secondOrderC1 = -cos(2 * double_Pi * (*currentCutOffFrequency)/sampleRate);
+}
--- Source/FilterProcessorBase.h
+++ Source/FilterProcessorBase.h
@@ -11,8 +11,38 @@
 #ifndef FILTERPROCESSORBASE_H_INCLUDED
 #define FILTERPROCESSORBASE_H_INCLUDED
 
+#include "../JuceLibraryCode/JuceHeader.h"
 
+class FilterProcessorBase: public AudioProcessor
 
-
+{
+public:
+  
+  
+private:
+  
+  AudioParameterFloat *currentCutOffFrequency, *currentBandwidth;
+  AudioParameterFloat *firstOrderC0;
+  AudioParameterFloat *secondOrderC0, *secondOrderC1;
+  
+  double sampleRate;
+  
+  struct AllpassState
+  {
+    double xh0,xh1,xh2; //xh0 current, xh1 at time n-1, xh2 at time n-2
+  };
+  
+  AllpassState currentAPState;
+  
+  void resetAllpassState();
+  void updateCoefficients();
+  
+  void applyFirstOrderAllpass(AudioBuffer<float>& buffer); //if we want our processor to support double precision we need to implement a second version of this
+  
+  void applySecondOrderAllpass(AudioBuffer<float>& buffer); //same here
+  
+  
+  
+};
 
 #endif  // FILTERPROCESSORBASE_H_INCLUDED
